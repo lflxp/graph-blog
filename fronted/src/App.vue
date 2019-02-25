0bd63512a210104aab84426787a53fcad12cfdd3
lixueping <382023823@qq.com>
2019-02-26 01:54:15 +0800

    init

--- fronted/src/App.vue
+++ fronted/src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <a-locale-provider :locale="locale">
+    <div id="app">
+      <router-view/>
+    </div>
+  </a-locale-provider>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+import { deviceEnquire, DEVICE_TYPE } from '@/utils/device'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      locale: zhCN
+    }
+  },
+  mounted () {
+    const { $store } = this
+    deviceEnquire(deviceType => {
+      switch (deviceType) {
+        case DEVICE_TYPE.DESKTOP:
+          $store.commit('TOGGLE_DEVICE', 'desktop')
+          $store.dispatch('setSidebar', true)
+          break
+        case DEVICE_TYPE.TABLET:
+          $store.commit('TOGGLE_DEVICE', 'tablet')
+          $store.dispatch('setSidebar', false)
+          break
+        case DEVICE_TYPE.MOBILE:
+        default:
+          $store.commit('TOGGLE_DEVICE', 'mobile')
+          $store.dispatch('setSidebar', true)
+          break
+      }
+      console.log('deviceType', deviceType)
+    })
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    height: 100%;
+  }
 </style>
---
+++ fronted/src/views/blog/test.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <a-button type="primary" @click="showDrawer">
+      Open
+    </a-button>
+    <a-drawer
+      title="Basic Drawer"
+      placement="right"
+      :closable="false"
+      @close="onClose"
+      :visible="visible"
+    >
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </a-drawer>
+    <a-divider type="vertical" />
+    <a href="https://vue.ant.design/components/drawer-cn/">官网</a>
+    <a-divider/>
+    <a-popconfirm title="Are you sure delete this task?" @confirm="confirm" @cancel="cancel" okText="Yes" cancelText="No">
+        <a href="#">Delete</a>
+    </a-popconfirm>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    showDrawer () {
+      this.visible = true
+    },
+    onClose () {
+      this.visible = false
+    },
+    confirm (e) {
+      console.log(e)
+      this.$message.success('Click on Yes')
+    },
+    cancel (e) {
+      console.log(e)
+      this.$message.error('Click on No')
+    }
+  }
+}
+</script>
